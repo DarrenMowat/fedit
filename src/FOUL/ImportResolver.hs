6339f43114084ce30d97a9aeea04c3aac5020de2
Darren Mowat <dmowat91@gmail.com>
2013-05-16 16:28:39 +0100

    Detect cyclic imports and bitch about them

--- src/FOUL/ImportResolver.hs
+++ src/FOUL/ImportResolver.hs
@@ -4,7 +4,7 @@ import FOUL.Parser
 import Util.FileUtils (splitPath)
 import Util.ListUtils (trimString)
 import System.FilePath ((</>))
-import Data.List (isSuffixOf)
+import Data.List (isSuffixOf, intercalate)
 import Paths_fedit (getDataFileName)
 
 stdLib :: [(String, String)]
@@ -15,20 +15,34 @@ data ImportTree = Branch (FilePath, String) [ImportTree] | Leaf (FilePath, Strin
 
 getDependencies :: FilePath -> IO (Either String [(FilePath, String)])
 getDependencies f = do 
-  tree <- makeImportTree f [] 
+  tree <- makeImportTree [] f
   case tree of 
   	Left e  -> return (Left e)
   	Right t -> return (Right (flattenImportTree t))
 
-makeImportTree :: FilePath -> [FilePath] -> IO (Either String ImportTree)
-makeImportTree root past = do 
+makeImportTree :: [FilePath] -> FilePath -> IO (Either String ImportTree)
+makeImportTree past root = do 
   contents <- readFile root
   let imps = getImports (lines contents)
   ifs <- mapM (getImportPath root) imps
-  case ifs of 
-    [] -> return $ Right (Leaf (root, contents))
-    otherwise -> do 
-      return (Left "")
+  case elem root past of 
+    True  -> return $ Left ("Cyclic import detected for " ++ root ++ " in " ++ (head past))
+    False -> case ifs of 
+      [] -> return $ Right (Leaf (root, contents))
+      otherwise -> do
+        mfs <- mapM (makeImportTree (root : past)) ifs
+        case collateErrors mfs of 
+          [] -> return (Right (Branch (root, contents) (collateBranches mfs)))
+          es -> return (Left (intercalate ", " es))
+
+collateErrors :: [Either String ImportTree] -> [String]
+collateErrors [] = [] 
+collateErrors ((Left e) : es) = e : collateErrors es
+collateErrors (e : es)        = collateErrors es
+
+collateBranches :: [Either String ImportTree] -> [ImportTree]
+collateBranches [] = [] 
+collateBranches ((Right i) : is) = i : collateBranches is
 
 getImportPath :: FilePath -> String -> IO FilePath
 getImportPath root name = case lookup (trimString name) stdLib of 
