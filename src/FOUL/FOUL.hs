fd683356ce09b2ce86c7dd6071558aa06993fff3
Darren Mowat <dmowat91@gmail.com>
2013-05-17 22:19:15 +0100

    Import solution for credit assignment

--- src/FOUL/FOUL.hs
+++ src/FOUL/FOUL.hs
@@ -1,10 +1,23 @@
-module FOUL.FOUL where
+module FOUL.FOUL(parseToFoul, evalMain, evalExpr) where
 
 import FOUL.ImportResolver
 import FOUL.Language
 import FOUL.Parser
 import Util.EitherUtils
 import Data.List (intercalate)
+import System.Environment (getArgs)
+
+{-
+	FOUL.FOUL is a completely standalone interpreter for FOUL
+	It has no dependencies on the fedit editor
+
+	The code below allows this to be compiled into the standalone 
+	FOUL interpreter. 
+	Just need change this to be the Main module of the program
+
+	Cabal is still required to build this due to the way the 
+	FOUL import system accesses standard library modules
+-}
 
 parseToFoul :: FilePath -> IO (Either String Prog)
 parseToFoul f = do 
@@ -22,3 +35,18 @@ evalMain prog = evalExpr prog (EA "main" [])
 
 evalExpr :: Prog -> Expr -> Either String Val 
 evalExpr p e = eval p [] e
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of 
+  	[] -> putStrLn "No input specified"
+  	xs -> go $ concat xs
+
+go :: FilePath -> IO ()
+go file = do
+  res <- parseToFoul file
+  case res of
+    Left err -> putStrLn $ "ERROR: " ++ err
+    Right prog -> putStrLn $ "\nmain() -> " ++ (show $ evalMain prog) ++ "\n"
+
--- src/Main.hs
+++ src/Main.hs
@@ -3,19 +3,3 @@ module Main where
 import System.Environment (getArgs)
 
 import Fedit
-import FOUL.FOUL
-
-main :: IO ()
-main = do
-  args <- getArgs
-  case args of 
-  	[] -> putStrLn "No input specified"
-  	xs -> parseProgram $ concat xs
-
-parseProgram :: FilePath -> IO ()
-parseProgram file = do
-  res <- parseToFoul file
-  case res of
-    Left err -> putStrLn $ "ERROR: " ++ err
-    Right prog -> putStrLn $ "Program() -> " ++ (show prog) ++ "\n\nmain() -> " ++ (show $ evalMain prog) ++ "\n"
-
